bd9f70d70f2bd82afe183414f3b3674a099357ee
Bandity <bandity9hard9@gmail.com>
2022-01-04 09:10:40 +0100

    Added the principal components to the app

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Team/>
+    <World/>
   </div>
 </template>
 
 <script>
-import Team from "./components/Team.vue";
+import Team from "./components/Team/Team.vue";
+import World from "./components/World/World.vue"
 
 export default {
   name: 'App',
   components: {
-    Team
+    Team,
+    World
   }
 }
 </script>
---
+++ src/src/components/Team/Team.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <PersoSelector/>
+      <Perso/>
+  </div>
+</template>
+
+<script>
+import PersoSelector from "../model./components/PersoSelector.vue";
+import Perso from "../model./components/Perso.vue";
+export default {
+    name: "Team",
+    components: {
+        PersoSelector,
+        Perso
+    }
+}
+</script>
+
+<style>
+
+</style>
