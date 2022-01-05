134667c9724defdc35584d92a60075d4ed787b7a
Bandity <bandity9hard9@gmail.com>
2022-01-05 10:26:15 +0100

    Personages updates

--- src/src/components/Team/Player/PersoCaracs.vue
+++ src/src/components/Team/Player/PersoCaracs.vue
@@ -13,7 +13,9 @@ export default {
     currentPlayer: Object
   },
   data:() =>{
-
+    return{
+      
+    };
   }
 };
 </script>
--- src/src/components/Team/Player/PersoSlots.vue
+++ src/src/components/Team/Player/PersoSlots.vue
@@ -4,8 +4,16 @@
 
 <script>
 export default {
-
-}
+  name: "PersoSlots",
+  props: {
+    currentPlayer: Object
+  },
+  data:() =>{
+    return{
+      
+    };
+  }
+};
 </script>
 
 <style>
